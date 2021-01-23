718a4a0e185bfe8dce02576b458c976729c38ea8
Alejandro del Castillo <alejandro.delcastillo@ni.com>
2021-01-22 18:38:21 -0600

    update to last version of prestosql
    Signed-off-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

---
+++ src/main/java/io/prestosql/plugin/vertica/VerticaClient.java
@@ -0,0 +1,75 @@
+package io.prestosql.plugin.vertica;
+
+import com.google.common.collect.ImmutableSet;
+import io.prestosql.plugin.jdbc.*;
+import io.prestosql.spi.PrestoException;
+import io.prestosql.spi.connector.ConnectorSession;
+import io.prestosql.spi.type.Type;
+
+import javax.inject.Inject;
+import java.sql.*;
+import java.util.Collection;
+import java.util.Optional;
+
+import static io.prestosql.plugin.jdbc.JdbcErrorCode.JDBC_ERROR;
+import static java.util.Locale.ENGLISH;
+
+public class VerticaClient extends BaseJdbcClient {
+
+    @Inject
+    public VerticaClient(BaseJdbcConfig config, ConnectionFactory connectionFactory) {
+        super(config, "", connectionFactory);
+    }
+
+    @Override
+    protected Collection<String> listSchemas(Connection connection) {
+        try(ResultSet resultSet = connection.getMetaData().getSchemas()) {
+            ImmutableSet.Builder<String> schemaNames = ImmutableSet.builder();
+            while (resultSet.next()) {
+                String schemaName = resultSet.getString("TABLE_SCHEM").toLowerCase(ENGLISH);
+                if (!schemaName.equals("v_monitor") && !schemaName.equals("v_txtindex") && !schemaName.equals("v_catalog")) {
+                    schemaNames.add(schemaName);
+                }
+            }
+            return schemaNames.build();
+        } catch (SQLException e) {
+            throw new PrestoException(JDBC_ERROR, e.getMessage());
+        }
+    }
+
+   @Override
+   protected ResultSet getTables(Connection connection, Optional<String> schemaName, Optional<String> tableName) throws SQLException {
+        DatabaseMetaData metadata = connection.getMetaData();
+        String escape = metadata.getSearchStringEscape();
+        return metadata.getTables(
+                connection.getCatalog(),
+                escapeNamePattern(schemaName, escape).orElse(null),
+                escapeNamePattern(tableName, escape).orElse(null),
+                new String[]{"TABLE"});
+    }
+
+    @Override
+    public PreparedStatement getPreparedStatement(Connection connection, String sql) throws SQLException {
+        connection.setAutoCommit(false);
+        PreparedStatement statement = connection.prepareStatement(sql);
+        return statement;
+    }
+
+    @Override
+    protected String getTableSchemaName(ResultSet resultSet) throws SQLException {
+        return resultSet.getString("TABLE_SCHEM").toLowerCase(ENGLISH);
+    }
+
+    @Override
+    public Optional<ColumnMapping> toPrestoType(ConnectorSession session, Connection connection, JdbcTypeHandle typeHandle) {
+        // TODO implement proper type mapping
+        return legacyToPrestoType(session, connection, typeHandle);
+    }
+
+    @Override
+    public WriteMapping toWriteMapping(ConnectorSession session, Type type) {
+        // TODO implement proper type mapping
+        return legacyToWriteMapping(session, type);
+    }
+
+}
---
+++ src/main/java/io/prestosql/plugin/vertica/VerticaClientModule.java
@@ -0,0 +1,35 @@
+package io.prestosql.plugin.vertica;
+
+import com.google.inject.Module;
+import com.google.inject.*;
+import com.vertica.jdbc.Driver;
+import io.prestosql.plugin.jdbc.*;
+import io.prestosql.plugin.jdbc.credential.CredentialProvider;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static io.airlift.configuration.ConfigBinder.configBinder;
+
+public class VerticaClientModule implements Module {
+
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(JdbcClient.class).annotatedWith(ForBaseJdbc.class).to(VerticaClient.class).in(Scopes.SINGLETON);
+        configBinder(binder).bindConfig(BaseJdbcConfig.class);
+        binder.install(new DecimalModule());
+    }
+
+    @Provides
+    @Singleton
+    @ForBaseJdbc
+    public static ConnectionFactory createConnectionFactory(BaseJdbcConfig config, CredentialProvider credentialProvider) throws SQLException  {
+        Properties connectionProperties = new Properties();
+        return new DriverConnectionFactory(
+                new Driver(),
+                config.getConnectionUrl(),
+                connectionProperties,
+                credentialProvider);
+    }
+
+}
